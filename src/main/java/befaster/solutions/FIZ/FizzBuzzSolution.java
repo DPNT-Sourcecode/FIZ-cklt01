59f28ccf5cd76314b8768e0d3aa8124336033488
sbx_user1051 <sbx_user1051@169.254.16.213>
2019-03-25 23:01:39 +0000

    Mon Mar 25 23:01:39 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,8 +4,17 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
 
+    private Boolean multipleThree(Integer number){
+        return ((number % 3) == 0);
+    }
+
+    private Boolean multipleFive(Integer number){
+        return ((number % 5) == 0);
+    }
+
     public String fizzBuzz(Integer number) {
         throw new SolutionNotImplementedException();
     }
 
 }
+
